5d05e3dc43aff0ad6189e99d52f566a00d595faf
hick-hpe <>
2025-10-30 19:39:35 -0300

    feat: tela de jogo atualizada

--- src/pages/TelaJogo.jsx
+++ src/pages/TelaJogo.jsx
@@ -1,6 +1,15 @@
-export default function TelaJogo() {
+import React from "react";
+import GameBoard from "../components/TelaJogo/GameBoard";
+import "../assets/css/global.css";
 
+export default function TelaJogo() {
   return (
-    <h1>Página do jogo</h1>
-  )
+    <div className="app">
+      <div className="titulo-ctn">
+        <h1 style={{fontSize: '60px'}}>♻️ Jogo do Lixo Eletrônico</h1>
+        <p>Arraste o item até a lixeira correta!</p>
+      </div>
+      <GameBoard />
+    </div>
+  );
 }
